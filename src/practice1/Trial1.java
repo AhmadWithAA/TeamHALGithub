eaf5cd532984b2f247b47c36aa3e49dde0709f9f
aghaz <aghaz@10.1.10.24>
2021-03-30 11:46:54 -0400

    newww

--- src/practice1/Trial1.java
+++ src/practice1/Trial1.java
@@ -7,6 +7,7 @@ public class Trial1 {
 		System.out.println("Papi tells me what to do please");
 		System.out.println(" papi bossy");
 		System.out.println("Responding to mami");
+		System.out.println("New Line");
 	}
 
 }

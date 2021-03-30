d5bb33eda7576337de963f2655cf2856b384925c
leidy <81108866+leidylu25@users.noreply.github.com>
2021-03-30 11:39:28 -0400

    new changes for trial class

--- src/practice1/Trial1.java
+++ src/practice1/Trial1.java
@@ -4,6 +4,8 @@ public class Trial1 {
 	
 	public static void main(String[] args) {
 		System.out.println("HELEN");
+		System.out.println("Papi tells me what to do please");
+		System.out.println(" papi bossy");
 	}
 
 }

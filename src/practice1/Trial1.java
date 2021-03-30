8fd6b4aeffa33817595e5a936c3115b67ecb747a
helen <helen@192.168.0.4>
2021-03-30 15:16:51 -0500

    Added print HELEN 2

--- src/practice1/Trial1.java
+++ src/practice1/Trial1.java
@@ -4,6 +4,9 @@ public class Trial1 {
 	
 	public static void main(String[] args) {
 		System.out.println("HELEN");
+		
+		
+		
 		System.out.println("Papi tells me what to do please");
 		System.out.println(" papi bossy");
 		System.out.println("Responding to mami");
@@ -11,6 +14,8 @@ public class Trial1 {
 
 		int age = 28;
 		System.out.println(age);
+		
+		System.out.println("HELEN 2");
 	}
 
 }
